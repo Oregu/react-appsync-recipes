6556364a2ac2161cb80cc9c0dba827289c15cd35
Oregu <oleg.burykin@gmail.com>
2018-04-08 10:54:47 -0700

    Initial commit, tutorial code

---
+++ README.md
@@ -0,0 +1,3 @@
+Tutorial: https://tylermcginnis.com/building-serverless-react-graphql-apps-with-aws-appsync/
+Download your appsync.js for Web from AWS AppSync console.
+
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import AWSAppSyncClient from "aws-appsync";
+import { Rehydrated } from 'aws-appsync-react';
+import { ApolloProvider } from 'react-apollo';
+
+import appSyncConfig from './appsync';
+
+// A
+const client = new AWSAppSyncClient({
+  url: appSyncConfig.graphqlEndpoint,
+  region: appSyncConfig.region,
+  auth: {
+    type: appSyncConfig.authenticationType,
+    apiKey: appSyncConfig.apiKey,
+  }
+});
+
+// B
+const WithProvider = () => (
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>
+);
+
+ReactDOM.render(<WithProvider />, document.getElementById('root'));
